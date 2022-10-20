2a30e6c2e20e842d1e77c59272dd8bffb759344c
makabaka1880 <makabaka1880@outlook.com>
2022-10-20 19:35:25 +0800

    testing dynamic app list

---
+++ appsLoaderScript.js
@@ -0,0 +1,20 @@
+function elementScript(data) {
+    `
+    <div id="HStack">
+        <img src="`+ data.image`" alt="Test"/>
+        <div>
+            <p class="p-heading-sans-thin" id="padding-5px"><b>` + data.name + `<b></p>
+            <p class="p-heading-sans-thin" id="padding-5px">` + data.description + `</p>
+        </div>
+        <div>
+            <a href="` + data.download + `" id="padding-5px"><i class="fa fa-download"></i></a>
+        </div>
+    </div>
+    `
+}
+const apps = JSON.parse(require('appsData.json'));
+var container = document.getElementById('')
+for app in apps {
+    console.log(app);
+    container.innerHTML += elementScript(app)
+}
